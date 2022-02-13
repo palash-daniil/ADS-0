065ffed9d32f45df36ba0ab673a99c4c639db2e6
palashdaniiI <99319050+palashdaniiI@users.noreply.github.com>
2022-02-14 00:44:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-if (b == 0)
-return a;
-return gcb(b, a  % b);
-return gcd(b, a  % b);
+if (a % b == 0)
+    return b;
+    if (b % a == 0)
+    return a;
+  if (a > b)
+    return gcd(a%b, b);
+  return gcd(a, b%a); 
 }
